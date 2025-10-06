9c64058e926322539df026db7829aea64ef03476
Ait Baatag <aitbaatagkhalid@gmail.com>
2025-10-06 10:37:02 +0100

    init

---
+++ etl-service/src/main/java/com/example/weather/App.java
@@ -0,0 +1,15 @@
+package com.example.weather;
+
+import com.example.weather.config.Config;
+import com.example.weather.service.WeatherApiClient;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+      WeatherApiClient client = new WeatherApiClient();
+
+      client.fetchWeatherData(Config.DEFAULT_LAT, Config.DEFAULT_LON);
+    }
+}
---
+++ etl-service/src/main/java/com/example/weather/model/WeatherData.java
@@ -0,0 +1,6 @@
+public class WeatherData {
+    private String city;
+    private double rainfallMm;
+    private String timestamp;
+    // getters and setters
+}
